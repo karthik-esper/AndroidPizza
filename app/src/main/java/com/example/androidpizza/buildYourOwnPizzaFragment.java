57a16db82702c3bb797c3905fc40324b9b5e2d36
Vineal Sunkara <vineal101@gmail.com>
2023-12-09 19:38:47 -0500

    BYOP almost done working on price and clearing items.

--- app/src/main/java/com/example/androidpizza/buildYourOwnPizzaFragment.java
+++ app/src/main/java/com/example/androidpizza/buildYourOwnPizzaFragment.java
@@ -35,8 +35,8 @@ public class buildYourOwnPizzaFragment extends Fragment {
     private RadioButton tomato;
     private RadioButton alfredo;
     private Spinner sizes;
-    private CheckBox extraSauce;
-    private CheckBox extraCheese;
+    private CheckBox exSauce;
+    private CheckBox exCheese;
     private Button placeOrder;
     private final int pizzaImages = R.drawable.byozza;
     private ArrayList<CheckBox> checkBoxList;
@@ -47,20 +47,16 @@ public class buildYourOwnPizzaFragment extends Fragment {
     private boolean sizeSelected = false;
     private static final int MAX_TOPPING_SIZE = 7;
     private static final int MIN_TOPPING_SIZE = 3;
+    private boolean extraCheeseAdded;
+    private boolean extraSauceAdded;
 
-    //    checkBoxList = new ArrayList<CheckBox>();
-//        checkBoxList.add(sausage);
-//        checkBoxList.add(pepperoni);
-//        checkBoxList.add(ham);
-//        checkBoxList.add(shrimp);
-//        checkBoxList.add(squid);
-//        checkBoxList.add(crab_meat);
-//        checkBoxList.add(green_pepper);
-//        checkBoxList.add(onion);
-//        checkBoxList.add(mushroom);
-//        checkBoxList.add(black_olive);
-//        checkBoxList.add(pineapple);
-//        checkBoxList.add(jalapeno);
+    private final double SMALL_PRICE = 8.99;
+    private final double MED_PRICE = 10.99;
+    private final double LG_PRICE = 12.99;
+    private final double ADD_TOP_PRICE = 1.49;
+    private final double EXTRAS_PRICE = 1.00;
+    private double currentPrice = SMALL_PRICE;
+
     public buildYourOwnPizzaFragment() {
 
     }
@@ -77,7 +73,11 @@ public class buildYourOwnPizzaFragment extends Fragment {
         spinner.setAdapter(adapter);
         setSpinnerListener(view);
         setToppingsListener(view);
+        setCheeseListener(view);
         sauceListener(view);
+        setSauceListener(view);
+        pricePrintListener(view);
+        createPizzaButton(view);
         return view;
     }
 
@@ -212,6 +212,7 @@ public class buildYourOwnPizzaFragment extends Fragment {
                     toppingRemove(Topping.JA, jalapeno);
             }
         });
+        //pricePrint(view);
     }
 
 
@@ -294,6 +295,7 @@ public class buildYourOwnPizzaFragment extends Fragment {
                 }
             }
         });
+
     }
 
     private void setSpinnerListener(View view) {
@@ -308,18 +310,24 @@ public class buildYourOwnPizzaFragment extends Fragment {
              */
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    CharSequence selectedValue = (CharSequence) parent.getItemAtPosition(position);
-                    Pizza toMake = createPizza("byop");
-                    char size = selectedValue.charAt(0);
-                    if (size == 's' || size == 'S') {toMake.setSize(Size.S);}
-                    if (size == 'm' || size == 'M') {toMake.setSize(Size.M);}
-                    if (size == 'l' || size == 'L') {toMake.setSize(Size.L);}
-                    selectedPizzaSize = String.valueOf(size);
-                    sizeSelected = true;
-                    Log.i("Size", selectedPizzaSize);
-                    //extraSauce.setChecked(false);
-                    //extraCheese.setChecked(false);
-                    //price.setText(String.format("%.2f", toMake.price()));
+                CharSequence selectedValue = (CharSequence) parent.getItemAtPosition(position);
+                Pizza toMake = createPizza("byop");
+                char size = selectedValue.charAt(0);
+                if (size == 's' || size == 'S') {
+                    toMake.setSize(Size.S);
+                }
+                if (size == 'm' || size == 'M') {
+                    toMake.setSize(Size.M);
+                }
+                if (size == 'l' || size == 'L') {
+                    toMake.setSize(Size.L);
+                }
+                selectedPizzaSize = String.valueOf(size);
+                sizeSelected = true;
+                Log.i("Size", selectedPizzaSize);
+                //extraSauce.setChecked(false);
+                //extraCheese.setChecked(false);
+                //price.setText(String.format("%.2f", toMake.price()));
             }
 
             /**
@@ -330,34 +338,205 @@ public class buildYourOwnPizzaFragment extends Fragment {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+        //pricePrint(view);
     }
 
-    private void createPizzaListener(View view) {
+    /**
+     * Handles the checkbox that selects whether the pizza has extra cheese or not with live updates.
+     * @param view the view that everything is handled on to fetch elements.
+     */
+    private void setCheeseListener(View view) {
+        CheckBox exSauce = (CheckBox) view.findViewById(R.id.exSauce);
+        CheckBox exCheese = (CheckBox) view.findViewById(R.id.exCheese);
+        exCheese.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Handles the selection and deselection of checkbox for extra cheese.
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                if (exCheese.isChecked()) {
+                    Pizza toMake = createPizza("byop");
+                    toMake.setSize(Size.valueOf(selectedPizzaSize));
+                    if (exCheese.isChecked()) {
+                        toMake.setExtraSauce(true);
+                    }
+                    toMake.setExtraCheese(true);
+                    //Price.setText("Price: " + String.format("%.2f", toMake.price()));
+                }
+                if (!exCheese.isChecked()) {
+                    Pizza toMake = createPizza("byop");
+                    toMake.setSize(Size.valueOf(selectedPizzaSize));
+                    if (exSauce.isChecked()) {
+                        toMake.setExtraSauce(true);
+                    }
+                    toMake.setExtraCheese(false);
+                    //Price.setText("Price: " + String.format("%.2f", toMake.price()));
+                }
+            }
+        });
+        //pricePrint(view);
+    }
+
+    /**
+     * Handles the checkbox that selects whether the pizza has extra sauce or not with live updates.
+     * @param view the view that everything is handled on to fetch elements.
+     */
+    private void setSauceListener(View view) {
+        CheckBox exSauce = (CheckBox) view.findViewById(R.id.exSauce);
+        CheckBox exCheese = (CheckBox) view.findViewById(R.id.exCheese);
+        exSauce.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Handles the selection and deselection of checkbox for extra sauce.
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                if (exSauce.isChecked()) {
+                    Pizza toMake = createPizza("byop");
+                    toMake.setSize(Size.valueOf(selectedPizzaSize));
+                    if (exCheese.isChecked()) {
+                        toMake.setExtraCheese(true);
+                    }
+                    toMake.setExtraSauce(true);
+                    //Price.setText("Price: " + String.format("%.2f", toMake.price()));
+                }
+                if (!exSauce.isChecked()) {
+                    Pizza toMake = createPizza("byop");
+                    toMake.setSize(Size.valueOf(selectedPizzaSize));
+                    if (exCheese.isChecked()) {
+                        toMake.setExtraSauce(true);
+                    }
+                    toMake.setExtraSauce(false);
+                    //Price.setText("Price: " + String.format("%.2f", toMake.price()));
+                }
+            }
+        });
+        //pricePrint(view);
+    }
+
+    protected void pricePrintListener(View view) {
+        TextView price = (TextView) view.findViewById(R.id.price);
+        CheckBox exSauce = (CheckBox) view.findViewById(R.id.exSauce);
+        CheckBox exCheese = (CheckBox) view.findViewById(R.id.exCheese);
+
         if(sizeSelected) {
-            Pizza toMake = createPizza("byop");
-            toMake.setToppings(toppers);
-            if (selectedPizzaSize.charAt(0) == (Size.S.getPizzaSize().charAt(0))) {
-                toMake.setSize(Size.S);
+            if(selectedPizzaSize.equals("S")) {
+                currentPrice = SMALL_PRICE;
             }
-            if (selectedPizzaSize.charAt(0) == (Size.M.getPizzaSize().charAt(0))) {
-                toMake.setSize(Size.M);
+            if(selectedPizzaSize.equals("S")) {
+                currentPrice = MED_PRICE;
             }
-            if (selectedPizzaSize.charAt(0) == (Size.L.getPizzaSize().charAt(0))) {
-                toMake.setSize(Size.L);
+            if(selectedPizzaSize.equals("S")) {
+                currentPrice = LG_PRICE;
             }
-            toMake.setExtraCheese(false);
-            toMake.setExtraSauce(false);
-            Store.getInstance().getCurrentOrder().addToOrder(toMake);
-            price.setText("Price: ");
-            Toast.makeText(getContext(), "Creating Pizza", Toast.LENGTH_LONG).show();
-            sizeSelected = false;
-            createAlert("Pizza Made");
-        }
-        else {
-            createAlert("No Size");}
         }
 
+        if (toppers.size() > MIN_TOPPING_SIZE) {
+            currentPrice += ADD_TOP_PRICE * (toppers.size() - MIN_TOPPING_SIZE);
+        }
+
+        if (exCheese.isSelected()) {
+            currentPrice += EXTRAS_PRICE;
+        }
+
+        if (exSauce.isSelected()) {
+            currentPrice += EXTRAS_PRICE;
+        }
+
+        price.setText("" + currentPrice);
     }
+
+    /**
+     * Clears certain boxes after the Pizza Button works.
+     * @param view the view that everything is handled on to fetch elements.
+     */
+    private void clearitems(View view) {
+        TextView price = (TextView) view.findViewById(R.id.price);
+        RadioGroup sauceGroup = (RadioGroup) view.findViewById(R.id.sauceGroup);
+        RadioButton tom = (RadioButton) view.findViewById((R.id.tomato));
+        RadioButton alf = (RadioButton) view.findViewById((R.id.alfredo));
+        CheckBox exSauce = (CheckBox) view.findViewById(R.id.exSauce);
+        CheckBox exCheese = (CheckBox) view.findViewById(R.id.exCheese);
+        CheckBox sausage = (CheckBox) view.findViewById(R.id.sausage);
+        CheckBox pepperoni = (CheckBox) view.findViewById(R.id.pepperoni);
+        CheckBox beef = (CheckBox) view.findViewById(R.id.beef);
+        CheckBox ham = (CheckBox) view.findViewById(R.id.ham);
+        CheckBox shrimp = (CheckBox) view.findViewById(R.id.shrimp);
+        CheckBox squid = (CheckBox) view.findViewById(R.id.squid);
+        CheckBox crab_meat = (CheckBox) view.findViewById(R.id.crab_meat);
+        CheckBox green_pepper = (CheckBox) view.findViewById(R.id.green_pepper);
+        CheckBox onion = (CheckBox) view.findViewById(R.id.onion);
+        CheckBox mushroom = (CheckBox) view.findViewById(R.id.mushroom);
+        CheckBox black_olive = (CheckBox) view.findViewById(R.id.black_olive);
+        CheckBox pineapple = (CheckBox) view.findViewById(R.id.pineapple);
+        CheckBox jalapeno = (CheckBox) view.findViewById(R.id.jalapeno);
+
+        exCheese.setChecked(false);
+        exSauce.setChecked(false);
+        sizes = view.findViewById(R.id.mySpinner);
+        sizes.setSelection(0);
+        sauceGroup.clearCheck();
+        sausage.setChecked(false);
+        pepperoni.setChecked(false);
+        beef.setChecked(false);
+        ham.setChecked(false);
+        shrimp.setChecked(false);
+        squid.setChecked(false);
+        crab_meat.setChecked(false);
+        green_pepper.setChecked(false);
+        onion.setChecked(false);
+        mushroom.setChecked(false);
+        black_olive.setChecked(false);
+        pineapple.setChecked(false);
+        jalapeno.setChecked(false);
+        price.setText("");
+    }
+
+    /**
+     * Handles the clicking of the createPizza button, which creates and adds a pizza to current order.
+     *
+     * @param view the view that everything is handled on to fetch elements.
+     */
+    private void createPizzaButton(View view) {
+        placeOrder = view.findViewById(R.id.createPizza);
+        placeOrder.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Handles the creation of a pizza and addition to order once button is clicked.
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                Pizza toMake = createPizza("byop");
+                toMake.setToppings(toppers);
+                if (selectedPizzaSize.charAt(0) == (Size.S.getPizzaSize().charAt(0))) {
+                    toMake.setSize(Size.S);
+                }
+                if (selectedPizzaSize.charAt(0) == (Size.M.getPizzaSize().charAt(0))) {
+                    toMake.setSize(Size.M);
+                }
+                if (selectedPizzaSize.charAt(0) == (Size.L.getPizzaSize().charAt(0))) {
+                    toMake.setSize(Size.L);
+                }
+                toMake.sauce = sauce;
+                toMake.setExtraCheese(extraCheeseAdded);
+                toMake.setExtraSauce(extraSauceAdded);
+                //price
+                Log.i("Pizza", toMake.toString());
+                //price.setText("Price: ");
+                Store.getInstance().getCurrentOrder().addToOrder(toMake);
+                createAlert("Pizza Made");
+                //clearitems(view);
+                if (!sizeSelected) {
+                    Toast.makeText(getContext(), "size not selected", Toast.LENGTH_SHORT).show();
+                    createAlert("No Size");
+                }
+            }
+
+        });
+    }
+}
+
 
 
 
